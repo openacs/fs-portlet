81a4bc14bf7c807a08371f50a4ff8f9f02e7587f
Yonatan Feldman <yon@milliped.com>
2001-11-14 21:59:53 +0000

    rem

--- sql/oracle/fs-portlet-create.sql
+++ sql/oracle/fs-portlet-create.sql
@@ -18,8 +18,6 @@ declare
   ds_id portal_datasources.datasource_id%TYPE;
 begin
   ds_id := portal_datasource.new(
-    data_type        => 'tcl_proc',
-    mime_type        => 'text/html',
     name             => 'fs-portlet',
     link             => 'file-storage',
     description      => 'Displays the given folder_id ',
