4c9d3cf7ea9ec0f0999c18773e1d46b4f6d842fb
Rocael Hernández Rizzardini <roc@viaro.net>
2004-08-30 05:32:30 +0000

    i18n File Storage Administration

--- tcl/fs-admin-portlet-procs.tcl
+++ tcl/fs-admin-portlet-procs.tcl
@@ -33,7 +33,7 @@ namespace eval fs_admin_portlet {
 
     ad_proc -public get_pretty_name {
     } {
-        return "File Storage Administration"
+        return "[_ fs-portlet.lt_File_Storage_Administ]"
     }
 
     ad_proc -private my_package_key {
