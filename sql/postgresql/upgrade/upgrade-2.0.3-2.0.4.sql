ebb89450e721de052bfa46ffa837d33b4771e65c
Hector Amado <hr_amado@galileo.edu>
2004-09-13 17:23:41 +0000

    fixing syntax error, by David Ortega doa@tid.es

--- sql/postgresql/upgrade/upgrade-2.0.3-2.0.4.sql
+++ sql/postgresql/upgrade/upgrade-2.0.3-2.0.4.sql
@@ -5,10 +5,6 @@
 -- fs_contents_portlets get renamed properly.
 --
 
---create or replace trigger fs_cont_port_fldr_rnme_tr
---after update on cr_folders
---for each row
-
 create or replace function fold_rename() returns trigger as '
 declare
   row_res record;
@@ -18,10 +14,10 @@ begin
     for row_res in select m.element_id
                 from portal_element_map m,
                 portal_element_parameters p
-  	        where p.key = "folder_id"
+  	        where p.key = ''folder_id''
                   and p.value = new.folder_id
                   and m.element_id = p.element_id
-                  and m.name = "fs_contents_portlet" loop
+                  and m.name = ''fs_contents_portlet'' loop
   
       update portal_element_map
       set pretty_name = new.label
@@ -35,6 +31,5 @@ end; ' language 'plpgsql';
 create trigger fs_cont_port_fldr_rnme_tr
 after update on cr_folders for each row
 execute procedure fold_rename();
--- drop function fold_rename();
 
 \i ../fs-admin-portlet-create.sql
