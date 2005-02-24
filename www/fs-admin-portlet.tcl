8d7b4761b898595480b9e26d97354139deeababf
Jeff Davis <davis@xarg.net>
2005-02-24 13:33:20 +0000

    commit to HEAD of merge of changes on oacs-5-1 branch between tags jcd-merge-5-1-20050111 and jcd-merge-5-1-20050224

--- www/fs-admin-portlet.tcl
+++ www/fs-admin-portlet.tcl
@@ -40,3 +40,4 @@ ad_return_template
 
 set package_id [site_node_apm_integration::get_child_package_id -package_key "file-storage"]
 set fs_url "/shared/parameters?[export_url_vars package_id return_url]"
+set show_fs_url_p [parameter::get_from_package_key -parameter ShowParametersLinkP -package_key [fs_portlet::my_package_key] -default 1]
