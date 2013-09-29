b7719c47b8123c3b17437c501e1094bf29a7be97
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-29 13:54:10 +0000

    - replace depreacted "ad_parameter" by parameter::get - replace deprecated "ad_permission_p" by "permission::permission_p" - replace deprecated "export_url_vars" by "export_vars"

--- www/fs-admin-portlet.tcl
+++ www/fs-admin-portlet.tcl
@@ -39,5 +39,5 @@ set user_id [ad_conn user_id]
 ad_return_template
 
 set package_id [site_node_apm_integration::get_child_package_id -package_key "file-storage"]
-set fs_url "/shared/parameters?[export_url_vars package_id return_url]"
+set fs_url "/shared/parameters?[export_vars -url {package_id return_url}]"
 set show_fs_url_p [parameter::get_from_package_key -parameter ShowParametersLinkP -package_key [fs_portlet::my_package_key] -default 1]
