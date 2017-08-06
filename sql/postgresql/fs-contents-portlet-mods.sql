2371b36c6751d3e64b7ac95592a0bd18d19490f1
cvs2svn <oct@openacs.org>
2017-08-06 11:17:34 +0000

    This commit was manufactured by cvs2svn to create tag 'openacs-5-9-compat'.
    Sprout from oacs-5-9 2017-08-06 11:17:32 UTC Gustaf Neumann  <gustaf.neumann@wu-wien.ac.at> '- do not depend on beta or development versions'
    Cherrypick from oacs-5-9 2015-07-14 22:36:42 UTC cvs2svn <oct@openacs.org> 'This commit was manufactured by cvs2svn to create branch 'oacs-5-9'.':
        sql/oracle/fs-contents-portlet-mods.sql
        sql/postgresql/fs-contents-portlet-mods.sql

---
+++ sql/postgresql/fs-contents-portlet-mods.sql
@@ -0,0 +1,33 @@
+--
+-- Created by aegrumet@alum.mit.edu on 2002-06-24.
+-- 
+-- This is a temporary workaround to ensure that
+-- fs_contents_portlets get renamed properly.
+--
+
+create or replace trigger fs_cont_port_fldr_rnme_tr
+after update on cr_folders
+for each row
+begin
+
+  if :old.label <> :new.label then
+
+    for row in (select m.element_id
+                from portal_element_map m,
+                portal_element_parameters p
+  	      where p.key = 'folder_id'
+                  and p.value = :new.folder_id
+                  and m.element_id = p.element_id
+                  and m.name = 'fs_contents_portlet') loop
+  
+      update portal_element_map
+      set pretty_name = :new.label
+      where element_id = row.element_id;
+  
+    end loop;
+
+end if;
+
+end fs_cont_port_fldr_rnme_tr;
+/
+show errors
